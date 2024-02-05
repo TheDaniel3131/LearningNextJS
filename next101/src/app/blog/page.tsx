9b7ed643aec389698e443295ec4ca2cb03bcef42
zephrous <danielpoh3131@gmail.com>
2024-02-05 16:28:07 +0800

    u date

---
+++ next101/src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div>
+      My Daily Blog
+    </div>
+  )
+}
+
+export default BlogPage
---
+++ next101/src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div>
+      Contact Me
+    </div>
+  )
+}
+
+export default ContactPage
